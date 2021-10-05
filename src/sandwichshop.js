1bb866e04c5574e926c09d6f33160c5c835e1673
yatsun <yatsunlam25@gmail.com>
2021-10-05 01:35:12 -0400

    added constructor

---
+++ src/sandwichshop.js
@@ -0,0 +1,9 @@
+class Sandwichshop {
+    constructor({description, id, name, city_id, city}){
+        this.description = description
+        this.id = id
+        this.name = name
+        this.city_id = city_id
+        this.city = city
+    }
+}
