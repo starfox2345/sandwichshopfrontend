ee0f44170814329f2c641c5ac2535bd540dea509
yatsun <yatsunlam25@gmail.com>
2021-10-05 01:31:29 -0400

    added css

--- src/sandwichshop_service.js
+++ src/sandwichshop_service.js
@@ -4,8 +4,8 @@ class SandwichshopService {
     }
 
     getSandwichshops(){
-        fetch(this.port + "/sandwichshops")
-        // fetch("http://localhost:3000/sandwichshops")
+        // fetch(this.port + "/sandwichshops")
+        fetch("http://localhost:3000/sandwichshops")
         .then(resp => resp.json())
         .then(data => console.log(data))
         .catch()
