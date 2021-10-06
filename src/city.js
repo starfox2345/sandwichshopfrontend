750c9d95aca2c0a3eb4835f75b1cbd1f28e47f24
yatsun <yatsunlam25@gmail.com>
2021-10-06 01:11:29 -0400

    added City class and constructor

---
+++ src/city.js
@@ -0,0 +1,10 @@
+class City {
+    constructor({name, state, zipcode, description, id, sandwichshops }){
+        this.name = name
+        this.state = state
+        this.zipcode = zipcode
+        this.description = description
+        this.id = id
+        this.sandwichshops = sandwichshops
+    }
+}
