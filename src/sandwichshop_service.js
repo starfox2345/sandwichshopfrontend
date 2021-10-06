0fea4e755d95339edbef36a63b3645d6f2fe7b5c
yatsun <yatsunlam25@gmail.com>
2021-10-06 03:39:22 -0400

    still fixing port issue bug

--- src/sandwichshop_service.js
+++ src/sandwichshop_service.js
@@ -58,7 +58,7 @@ class SandwichshopService {
 
         }
         debugger
-        fetch("http://localhost:3000/sandwichshops/${id}", configObject)
+        fetch(this.port + `/sandwichshops/${id}`, configObject)
         .then(resp => resp.json())
         .then(data => {
             // const s = new Sandwichshop(data)
