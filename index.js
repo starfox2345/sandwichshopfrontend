cc11de6d5bb551e7b7451fb9f811f2baf2b07242
yatsun <yatsunlam25@gmail.com>
2021-10-14 23:22:37 -0400

    done

--- index.js
+++ index.js
@@ -20,28 +20,3 @@ function handleSubmit(e){
 
 sandwichshopCall.getSandwichshops();
 cityCall.getCities();
-
-// ----------------------------------------------------
-
-
-// const port = 'http://localhost:3000';
-// const sandwichshopService = new SandwichshopService(port);
-// const cityService = new CityService(port);
-// const dropdown = document.getElementById('cit-dropdown')
-// const cityInput = document.getElementById('city-id')
-// const ul = document.getElementById('sandwichshop-list');
-// const form = document.getElementById('sandwichshop-form');
-// const nameInput = document.getElementById('sandwichshop-name');
-// const descriptionInput = document.getElementById('sandwichshop-description');
-// const baseURL = 'http://localhost:3000/sandwichshops'
-// form.addEventListener('submit', handleSubmit)
-
-// function handleSubmit(e){
-//     e.preventDefault();
-//     sandwichshopService.createSandwichshops();
-//     e.target.reset();
-    
-// }
-
-// sandwichshopService.getSandwichshops()
-// cityService.getCities()
--- script.js
+++
@@ -1,5 +0,0 @@
-let btn1 = document.querySelector('#btn-1');
-
-btn1.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('https://www.tripsavvy.com/thmb/yG6UCI6s2pYNUTmiMLmqTvEIqFc=/960x494/filters:no_upscale():max_bytes(150000):strip_icc()/10347248_671018469656277_2967798082207803337_n-58e7a1de3df78c51624bce83.jpg')"
-})
